fa6865a4b06646e9f4ebe1e315b5d4990ebdcce2
Taishawn King <taishawnking1@yahoo.com>
2019-12-15 05:31:18 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
+#require "pry"
 def using_until
-  levitation_force = 6
-  #your code here
-    
-end
+levitation_force = 6  
+ until levitation_force == 10   
+ puts  "Wingardium Leviosa"   
+  levitation_force += 1  
+ # binding.pry
+   end 
+ end
 
